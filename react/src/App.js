8bb834ccfa7208bef3e675f5b1fd1d2365fd8a05
IbragimKhaniev <newinterny@gmail.com>
2025-07-23 22:08:21 +0300

    Update files

--- react/src/App.js
+++ react/src/App.js
@@ -1,15 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ErrorBoundary from './ErrorBoundary';
+import Registration from './components/Registration';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import Feed from './components/Feed';
 import './App.css';
 
 function App() {
   return (
     <ErrorBoundary>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/register" element={<Registration />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/feed" element={<Feed />} />
+        </Routes>
+      </Router>
     </ErrorBoundary>
   );
 }
---
+++ react/src/components/Feed.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Feed() {
+  return (
+    <div>
+      <h1>Лента</h1>
+    </div>
+  );
+}
+
+export default Feed;
